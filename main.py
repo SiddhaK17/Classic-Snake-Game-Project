64d287fd4fcfcdc95e4734f84f4ea8121aaa090c
SiddhaK17 <siddhakad17@gmail.com>
2025-08-12 19:45:29 +0530

    feat: add initial implementation of main.py with core game loop
    - Set up Turtle Graphics screen and game window
    - Established game loop with real time updates
    - Prepared foundation for snake movement and collision handling

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+
+from scoreboard import Scoreboard
+
+os.environ['TCL_LIBRARY'] = r'C:\Program Files\Python313\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Program Files\Python313\tcl\tk8.6'
+
+from turtle import Turtle, Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)         #when you write this, nothing will be shown on the screen untill you write screen.update()
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+
+    #Detect collision with tail.
+    for segment in snake.segments:   # or you can write "for segment in snake.segments[1:]:"
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+    #if head collides with any segment in the tail:
+        #trigger game_over
+
+
+screen.exitonclick()
