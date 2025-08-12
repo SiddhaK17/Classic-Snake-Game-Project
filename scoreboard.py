2d3d3b72bcea157ae98422c4b62aa37da83137aa
SiddhaK17 <siddhakad17@gmail.com>
2025-08-12 19:54:24 +0530

    feat: add scoreboard.py with live score tracking
    - Implemented real time score updates
    - Displayed score using Turtle text rendering
    - Structured for future high score persistence

---
+++ scoreboard.py
@@ -0,0 +1,43 @@
+import os
+
+os.environ['TCL_LIBRARY'] = r'C:\Program Files\Python313\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Program Files\Python313\tcl\tk8.6'
+
+from turtle import Turtle, Screen
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+class Scoreboard(Turtle):
+# The below things will make sure that the program would work even if the file storing the high score is empty
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:         #saving the file to a variable data
+            self.high_score = int(data.read())
+        self.color("white")
+        self.penup()
+        self.goto(0, 265)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score} ", align=ALIGNMENT, font=FONT)
+
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(f"GAME OVER:", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
